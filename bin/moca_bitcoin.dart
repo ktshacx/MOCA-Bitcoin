4d4a63c2cbdb6b3992e98a4121fcd51340a6939e
Krish Tech Studio <krishnendumaityr@gmail.com>
2024-02-27 15:01:57 +0530

    update 4

--- bin/moca_bitcoin.dart
+++ bin/moca_bitcoin.dart
@@ -1,5 +1,17 @@
 import 'package:moca_bitcoin/moca_bitcoin.dart';
 
 void main(List<String> arguments) {
-  print(MOCABitcoin.generateMnemonic());
+  var mnemonic = MOCABitcoin.generateMnemonic();
+  print('Mnemonic: ${mnemonic}');
+
+  var publicKey = MOCABitcoin.getPublicKey(mnemonic);
+  print('Public Key: ${publicKey.toString()}');
+
+  var address = MOCABitcoin.getAddress(publicKey);
+  print('Address: ${address}');
+
+  MOCABitcoin.getBalance(address).then((balance) => {
+    print('Balance: ${balance} BTC')
+  });
+  
 }
